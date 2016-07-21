196f06729de978b10892346d13dcb9e2ef26072b
jpcp13 <jpcp13@gmail.com>
2016-07-22 00:38:38 +0200

    test if png files are committed

--- python/bezout.py
+++ python/bezout.py
@@ -13,7 +13,7 @@ Btri = bm.block_triang(deg, B)
 plt.subplot(1, 2, 1)
 plt.spy(B[0]); plt.grid()
 plt.subplot(1, 2, 2)
-plt.spy(Btri[0]); plt.grid()
+plt.spy(B[0]); plt.grid()
 plt.savefig('../png/B0_B0tri.png')
 
 bm._save2text(B)
